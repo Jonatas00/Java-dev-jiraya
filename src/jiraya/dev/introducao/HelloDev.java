9839f020b529ae0c730431b5e0821dc841e22a18
jonatas <djonatas31@gmail.com>
2024-06-03 22:21:42 -0300

    Initial commit

---
+++ src/jiraya/dev/introducao/HelloDev.java
@@ -0,0 +1,15 @@
+package jiraya.dev.introducao;
+
+/**
+ * Javadoc <strong>comment</strong>
+ */
+public class HelloDev {
+    // One line comment
+
+    /*
+        Multiple lines comment
+    */
+    public static void main(String[] args) {
+        System.out.print("Hello and welcome dev!");
+    }
+}
---
+++ src/jiraya/dev/introducao/PrimitiveTypes.java
@@ -0,0 +1,20 @@
+package jiraya.dev.introducao;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        String name = "Jonatas";
+        int age = 21;
+        long bigNum = 100000;
+        double height = 1.78;
+        float salary = 1000.00F;
+        byte ageByte = 127;
+        short ageShort = 21;
+        boolean ehtrue = true;
+        boolean ehfalse = false;
+        char ypsulum = '\u0059';
+
+        System.out.println(ypsulum);
+
+        System.out.println(height);
+    }
+}
