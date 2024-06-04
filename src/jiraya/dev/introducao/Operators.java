a105bc60bce3fee07d3e03794a43fc04d9f28021
jonatas <djonatas31@gmail.com>
2024-06-04 20:22:40 -0300

    add operators

--- src/jiraya/dev/introducao/Operators.java
+++ src/jiraya/dev/introducao/Operators.java
@@ -7,23 +7,46 @@ public class Operators {
         int num1 = 10;
         int num2 = 20;
 
+        /*
         System.out.println(num1 + num2);
         System.out.println(num1 - num2);
         System.out.println(num1 * num2);
         System.out.println(num1 / (double) num2);
 
         System.out.println("Valor:" + (num1 + num2));
+        */
 
         // %
 
         int module = 51 % 2;
 
-        System.out.println(module);
+        // System.out.println(module);
 
         // < > <= >= !=
 
         boolean is10m20 = 10 < 20;
 
-        System.out.println(is10m20);
+        // System.out.println(is10m20);
+
+        // AND (&&) or (||)
+
+        String gender = "M";
+        int age = 8;
+        /*
+
+        if (gender == "M" && age >= 18) {
+            System.out.printf("He is %s %d", gender, age);
+        } else if (gender == "F" || age <= 18) {
+            System.out.printf("She is %s %d", gender, age);
+        }
+        */
+
+        // += *=
+
+        int i = 1;
+        i++;
+        i--;
+        ++i;
+        System.out.println(i);
     }
 }
