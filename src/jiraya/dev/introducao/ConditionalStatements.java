15d2e6c149ff9ff9fe9b61449235482be7cbc165
jonatas <djonatas31@gmail.com>
2024-06-05 22:38:47 -0300

    switch statement

--- src/jiraya/dev/introducao/ConditionalStatements.java
+++ src/jiraya/dev/introducao/ConditionalStatements.java
@@ -22,8 +22,39 @@ public class ConditionalStatements {
         System.out.println(result);
 
         int age = 19;
-        String category = age <= 15 ? "C": age <= 17 ? "B" : age < 20 ? "A" : "No qualified";
+        String category = age <= 15 ? "C" : age <= 17 ? "B" : age < 20 ? "A" : "No qualified";
 
         System.out.println(category);
+
+        // Switch
+
+        byte day = 5;
+
+        switch (day) {
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 2:
+                System.out.println("Monday");
+                break;
+            case 3:
+                System.out.println("Tuesday");
+                break;
+            case 4:
+                System.out.println("Wednesday");
+                break;
+            case 5:
+                System.out.println("Thursday");
+                break;
+            case 6:
+                System.out.println("Friday");
+                break;
+            case 7:
+                System.out.println("Saturday");
+                break;
+            default:
+                System.out.println("Invalid");
+                break;
+        }
     }
 }
