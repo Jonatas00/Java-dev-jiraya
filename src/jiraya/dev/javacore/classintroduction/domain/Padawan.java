5e21b5172b0babd0e2485fd18679cc8df275f3cf
jonatas <djonatas31@gmail.com>
2024-06-07 00:02:59 -0300

    class introduction

---
+++ src/jiraya/dev/javacore/classintroduction/domain/Padawan.java
@@ -0,0 +1,13 @@
+package jiraya.dev.javacore.classintroduction.domain;
+
+public class Padawan {
+    public String name;
+    public int age;
+    public char gender;
+
+    public Padawan(String name, int age, char gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+}
---
+++ src/jiraya/dev/javacore/classintroduction/test/PadawanTest01.java
@@ -0,0 +1,15 @@
+package jiraya.dev.javacore.classintroduction.test;
+
+import jiraya.dev.javacore.classintroduction.domain.Padawan;
+
+public class PadawanTest01 {
+    public static void main(String[] args) {
+        Padawan pad01 = new Padawan("Anakin", 21, 'M');
+
+        Padawan pad02 = new Padawan("Luke", 15, 'M');
+
+        System.out.println(pad01.name);
+        System.out.println(pad01.age);
+        System.out.println(pad01.gender);
+    }
+}
